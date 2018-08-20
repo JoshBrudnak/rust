d110803c628ad3c7804a01886ef30ece51479bcf
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2018-08-20 07:18:30 +0300

    rustc: remove Ty::{is_self, has_self_ty}.

--- src/librustc_typeck/collect.rs
+++ src/librustc_typeck/collect.rs
@@ -919,10 +919,6 @@ fn generics_of<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>,
     let mut i = 0;
     params.extend(ast_generics.params.iter().filter_map(|param| match param.kind {
         GenericParamKind::Type { ref default, synthetic, .. } => {
-            if param.name.ident().name == keywords::SelfType.name() {
-                span_bug!(param.span,  "`Self` should not be the name of a regular parameter");
-            }
-
             if !allow_defaults && default.is_some() {
                 if !tcx.features().default_type_parameter_fallback {
                     tcx.lint_node(
